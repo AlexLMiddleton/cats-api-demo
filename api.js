c28d9ad5a194063fc85defc4d299c6fd3e87577b
Alex <alexmiddleton@outlook.com>
2018-02-05 14:15:23 -0500

    GET /cats

--- api.js
+++ api.js
@@ -7,4 +7,13 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('meow.'));
 
+const cats = [
+  { name: 'Mittens', age: 3, gender: 'Female', breed: 'Tabby' },
+  { name: 'Muffins', age: 2, gender: 'Male', breed: 'Siamese' },
+  { name: 'Mr. Handsome', age: 8, gender: 'Male', breed: 'Tom' },
+  { name: 'Miss Krunkles', age: 5, gender: 'Female', breed: 'Torty' }
+];
+
+app.get('/cats', (req, res) => res.send(cats));
+
 app.listen(port, () => console.log('CATS! ', port));
